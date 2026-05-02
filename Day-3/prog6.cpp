8bf0a2fd2c820404e71f046543a897cc0727a826
Vanshika Shekhawat <vanshikashekhawat76@gmail.com>
2026-05-02 20:01:28 +0530

    Added C++ file

---
+++ Day-3/prog6.cpp
@@ -0,0 +1,18 @@
+/*WAP to create a multiplication table using for loop*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+   cout << "Enter an integer: ";
+   cin >> n;
+
+    cout << "\nMultiplication Table for " << n << ":" <<endl;
+   
+    for (int i = 1; i <= 10; i++) {
+    cout << n << " x " << i << " = " << (n * i) <<endl;
+    }
+
+    return 0;
+}
